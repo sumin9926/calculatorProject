c70f2da91d55819c1e1fd41530be2f220840ab12
임PRO스터\su992 <su9926@naver.com>
2024-11-19 19:39:34 +0900

    :sparkles: new functions

--- Calculators/InputData.java
+++ Calculators/InputData.java
@@ -1,35 +1,34 @@
 package Calculators;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 class InputData {
-    Scanner sc=new Scanner(System.in);
-
-    /*양의 정수를 입력 받는 메서드*/
-    int inputPositiveInt(String seq){ //seq : 순서 입력
-        double value;
-        System.out.println(seq+" '양의 정수'를 입력하세요.");
-        while(true) {
-            value = sc.nextDouble();
-            sc.nextLine(); //버퍼에 남은 \n 제거
-            if (value % 1 != 0.0 || value < 0) {
-                System.out.println("'양의 정수'를 입력하세요.");
-            } else {
-                break; //입력된 수가 양의 정수인 경우 반복 종료
-            }
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    /*사용자로부터 피연산자를 입력 받는 메서드*/
+    Numbers<Double> inputNumbers() throws IOException {
+        try{
+            String stringTypeNum=br.readLine();
+            Numbers<Double> num=new Numbers<>();
+            num.setNumber(Double.valueOf(stringTypeNum));
+            return num;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (NumberFormatException n){
+            System.out.println("숫자 형식으로 입력하세요. ex) 3, 2.4");
+            return null;
         }
-        return (int)value; //double 타입을 int 타입으로 강제 형변환
+    }
+    /*입력받은 피연산자를 반환하는 메서드*/
+    Double getOperand() throws IOException {
+        Double operand=null;
+        while(operand==null){
+            operand = inputNumbers().getNumber();
+        }
+        return operand;
     }
 
     /*연산자를 입력받는 메서드*/
-    String inputOperator(){
-        String operator;
-        System.out.println("사칙연산 기호(+,-,*,/)를 입력하세요.");
-        while (true) {
-            operator = sc.nextLine();
-            if (operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/")) break;
-            else System.out.println("올바른 기호를 입력하세요.");
-        }
-        return operator;
-    }
+
 }
